8086427b1687bdd4aed2a3bd82244442364d8f6c
Dhruv Soni <sonidhruv557@gmail.com>
2025-06-19 02:27:59 +0530

    Initial commit - portfolio project

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8900/api",
+  baseURL: "https://portfolio-dhruv557.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
